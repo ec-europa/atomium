f9431f260f69bc399fcaae77833014f44894fd5a
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-03-03 18:22:05 +0100

    #7: Fix form submit and some glitches.

---
+++ atomium/templates/progress_bar/progress_bar.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<div id="progress" class="progress">
+    <div class="bar"><div class="filled" style="width: <?php print $variables['percent']; ?>%"></div></div>
+    <div class="percentage"><?php print $variables['percent']; ?></div>';
+    <div class="message"><?php print $variables['message']; ?></div>
+</div>
