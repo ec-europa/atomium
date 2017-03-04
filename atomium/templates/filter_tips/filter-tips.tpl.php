3a4c45908810d3ae00432b4023277235155ceb9b
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-03-04 16:51:59 +0100

    #7: Use item_list as much as we can.

--- atomium/templates/filter_tips/filter-tips.tpl.php
+++ atomium/templates/filter_tips/filter-tips.tpl.php
@@ -6,15 +6,4 @@
  */
 ?>
 <h2><?php print t('Text Formats');?></h2>
-<div class="compose-tips">
-  <?php foreach ($tips as $name => $tipList): ?>
-      <div class="filter-type filter-'<?php print drupal_html_class($name); ?>'">
-          <h3><?php print check_plain($name); ?></h3>
-          <ul class="tips">
-            <?php foreach ($tips[$name] as $tip): ?>
-                <li<?php print ($long ? ' id="filter-' . str_replace("/", "-", $tip['id']) . '"' : '');?>><?php print $tip['tip'];?></li>
-            <?php endforeach; ?>
-          </ul>
-      </div>
-  <?php endforeach; ?>
-</div>
+<?php print render($tips); ?>
