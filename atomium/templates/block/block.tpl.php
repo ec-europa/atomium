e519c0dee50e68242aeddbf428a90edf91be2a24
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-03-06 15:03:23 +0100

    #7: Fix a couple issues related to forms.

--- atomium/templates/block/block.tpl.php
+++ atomium/templates/block/block.tpl.php
@@ -7,14 +7,9 @@
 ?>
 <section<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
-  <?php if ($block->title): ?>
-    <h2<?php print $title_attributes; ?>><?php print $block->title ?></h2>
-  <?php endif;?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($content): ?>
-    <div<?php print $content_attributes; ?>>
-      <?php print render($content); ?>
-    </div>
+  <?php if ($title['#markup']): ?>
+    <?php print render($title); ?>
   <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php print render($content); ?>
 </section>
