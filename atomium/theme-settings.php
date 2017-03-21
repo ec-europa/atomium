037adb21bddb9ca4720ca6cefb90ad3d82bdb038
Pol Dellaiera <Pol@47194.no-reply.drupal.org>
2017-03-21 16:55:40 +0100

    #7: Update description of theme settings.

--- atomium/theme-settings.php
+++ atomium/theme-settings.php
@@ -16,14 +16,15 @@ function atomium_form_system_theme_settings_alter(&$form, $form_state) {
   );
   $form['atomium']['theme_debug'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('Theme debug'),
+    '#title'         => t('Enable theme debug ?'),
     '#default_value' => theme_get_setting('theme_debug'),
-    '#description'   => t('Enable theme debug ?'),
+    '#description'   => t('Theme debug mode can be used to see possible template suggestions and the locations of template files right in your HTML markup (as HTML comments). For more information, see <a href="@url">documentation</a>.', array('@url' => 'https://www.drupal.org/docs/7/theming/overriding-themable-output/working-with-template-suggestions')),
   );
   $form['atomium']['allow_css_double_underscores'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('Allow CSS double underscore'),
+    '#title'         => t('Allow CSS double underscore ?'),
     '#default_value' => theme_get_setting('allow_css_double_underscores'),
+    '#description' => t('In order to allow CSS identifiers to contain double underscores for Drupal\'s BEM-style naming standards, this variable can be set to TRUE. For more information, see <a href="@url">documentation</a>.', array('@url' => 'https://www.drupal.org/node/2810369')),
   );
   $form['atomium']['atomium_add_hook_classes'] = array(
     '#type'          => 'checkbox',
