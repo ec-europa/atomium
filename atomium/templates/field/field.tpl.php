603337c365c77dcf071c84c78ad459e208cda7b8
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-03 15:29:35 +0200

    Issue #338: Use an item_list component to render the fields.

--- atomium/templates/field/field.tpl.php
+++ atomium/templates/field/field.tpl.php
@@ -5,15 +5,4 @@
  * Contains template file.
  */
 ?>
-<div<?php print $atomium['attributes']['wrapper']; ?>>
-  <?php if (!$label_hidden) : ?>
-      <label<?php print $atomium['attributes']['title']; ?>><?php print $label ?></label>
-  <?php endif; ?>
-    <div<?php print $atomium['attributes']['content']->append('class', 'field-items'); ?>>
-      <?php foreach ($items as $delta => $item): ?>
-          <div<?php print $atomium['attributes'][$delta]->append('class', array('field-item', ($delta % 2 ? 'odd' : 'even'))); ?>>
-            <?php print render($item); ?>
-          </div>
-      <?php endforeach; ?>
-    </div>
-</div>
+<?php print render($field); ?>
