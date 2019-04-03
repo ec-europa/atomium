5e5654f7c7c2e92d06d80aedbaa4d4618005fbca
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-03 17:00:01 +0200

    Issue #338: Fix duplicated attributes in node template from Atomium Bartik theme.

--- atomium_bartik/templates/node/node.tpl.php
+++ atomium_bartik/templates/node/node.tpl.php
@@ -78,7 +78,7 @@
  * @see template_process()
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $atomium['attributes']['wrapper']; ?>>
+<div<?php print $atomium['attributes']['wrapper']; ?>>
 
   <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
