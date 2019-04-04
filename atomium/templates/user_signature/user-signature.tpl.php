6fba565f122c86a6350aef1687003d5ed5b00dae
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-04 15:24:46 +0200

    Issue #341: Add user_signature component.

---
+++ atomium/templates/user_signature/user-signature.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<?php if (!empty($variables['signature'])): ?>
+  <div class="clear">
+    <div>—</div>
+    <?php print $variables['signature']; ?>
+  </div>
+<?php endif; ?>
