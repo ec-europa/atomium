8288b8ec248041a703f217564426582eea554e7b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-11 21:37:00 +0200

    Revert "Issue #343: Solve anchor issue with breadcrumb at atomium-overview page and set vars in oreprocessor avoiding hardcode within tpl file."
    This reverts commit 69adfa78

--- atomium/templates/atomium_overview/atomium-overview.tpl.php
+++ atomium/templates/atomium_overview/atomium-overview.tpl.php
@@ -5,16 +5,23 @@
  * Contains template file.
  */
 ?>
-<?php print render($component_list) ?>
 
-<h3><?php print $h3_header ?></h3>
+<ul>
+  <?php foreach ($definitions as $name => $definition): ?>
+      <li>
+          <a href="#<?php print $name ?>"><?php print $definition['label'] ?></a>
+      </li>
+  <?php endforeach; ?>
+</ul>
+
+<h3><?php print t('Components') ?></h3>
 
 <div>
   <?php foreach ($definitions as $name => $definition): ?>
-      <a name="<?php print $definition['attribute_name'] ?>"></a>
+      <a name="<?php print $name ?>"></a>
       <h4><?php print $definition['label'] ?></h4>
       <fieldset>
-          <legend><?php print $fieldset_legend ?></legend>
+          <legend><?php print t('Preview') ?></legend>
           <div>
             <?php if (isset($definition['description'])): ?>
                 <p><?php print $definition['description'] ?></p>
