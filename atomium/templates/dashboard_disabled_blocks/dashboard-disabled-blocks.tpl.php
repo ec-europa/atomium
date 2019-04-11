97e85327840cd360d5fa06cddf76bb463d90b4dc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-11 15:38:52 +0200

    Issue #344: Update components (#345)
    * Issue #341: Update system_powered_by component.
    
    * Issue #341: Update dashboard_disabled_blocks component.

--- atomium/templates/dashboard_disabled_blocks/dashboard-disabled-blocks.tpl.php
+++ atomium/templates/dashboard_disabled_blocks/dashboard-disabled-blocks.tpl.php
@@ -6,13 +6,13 @@
  */
 ?>
 <div class="canvas-content">
-  <p><?php print t('Drag and drop these blocks to the columns below. Changes are automatically saved. More options are available on the <a href="@dashboard-url">configuration page</a>.', array('@dashboard-url' => url('admin/dashboard/configure'))); ?></p>
+  <p><?php print render($help_text); ?></p>
   <div id="disabled-blocks">
     <div class="region disabled-blocks clearfix">
       <?php print render($disabled_blocks); ?>
       <div class="clearfix"></div>
       <p class="dashboard-add-other-blocks">
-        <?php print l(t('Add other blocks'), 'admin/dashboard/configure'); ?>
+        <?php print render($add_other_blocks); ?>
       </p>
     </div>
   </div>
