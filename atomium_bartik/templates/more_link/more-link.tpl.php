0161c7a7afd3b0b15d33b5ac322660e7e34d5b89
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-17 15:07:20 +0200

    Issue #357: Make sure that atomium_bartik match Bartik core theme.

---
+++ atomium_bartik/templates/more_link/more-link.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Contains template file.
+ */
+?>
+<div<?php print $atomium['attributes']['wrapper']->append('class', 'more-link'); ?>>
+  <?php print render($more_link); ?>
+</div>
