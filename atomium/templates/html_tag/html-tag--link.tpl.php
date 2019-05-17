7426ab449f0848e64b5432eb1611f1033fd4725d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-05-17 17:24:57 +0200

    Remove html_tag component.

--- atomium/templates/html_tag/html-tag--link.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Contains template file.
- */
-?>
-<<?php print $element['#tag']; ?><?php print $atomium['attributes']['element']; ?>>
--- atomium/templates/html_tag/html-tag.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Contains template file.
- */
-?>
-<<?php print $element['#tag']; ?><?php print $atomium['attributes']['element']; ?>><?php print render($element['#value']); ?><?php print $element['#children']; ?></<?php print $element['#tag']; ?>>
