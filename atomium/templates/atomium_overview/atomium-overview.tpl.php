929f052981de0f0a6f72fdbfc3380715edc97fad
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-05-27 10:04:11 +0200

    Update atomium_overview component.

--- atomium/templates/atomium_overview/atomium-overview.tpl.php
+++ atomium/templates/atomium_overview/atomium-overview.tpl.php
@@ -7,9 +7,9 @@
 ?>
 
 <ul>
-  <?php foreach ($definitions as $name => $definition): ?>
+  <?php foreach ($definitions['#items'] as $name => $definition): ?>
       <li>
-          <a href="#<?php print $name ?>-preview"><?php print $definition['label'] ?></a>
+          <a href="#<?php print $definition['#name']; ?>-preview"><?php print $definition['#title'] ?></a>
       </li>
   <?php endforeach; ?>
 </ul>
@@ -17,27 +17,5 @@
 <h3><?php print t('Components') ?></h3>
 
 <div>
-  <?php foreach ($definitions as $name => $definition): ?>
-      <a name="<?php print $name ?>-preview"></a>
-      <h4><?php print $definition['label'] ?></h4>
-      <fieldset>
-          <legend><?php print t('Preview') ?></legend>
-          <div>
-            <?php if (isset($definition['description'])): ?>
-                <p><?php print $definition['description'] ?></p>
-            <?php endif; ?>
-            <?php if (isset($definition['preview'])): ?>
-                <div class="clearfix">
-                  <?php print render($definition['preview']) ?>
-                </div>
-            <?php endif; ?>
-            <?php if (isset($definition['form'])): ?>
-                <div class="clearfix">
-                  <?php print render($definition['form']) ?>
-                </div>
-            <?php endif; ?>
-
-          </div>
-      </fieldset>
-  <?php endforeach; ?>
+  <?php print render($definitions); ?>
 </div>
