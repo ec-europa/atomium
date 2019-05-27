d37fde69049a923575cac89d80722465bbbdc443
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-05-27 11:50:31 +0200

    Update component component.

--- atomium/templates/component/component.tpl.php
+++ atomium/templates/component/component.tpl.php
@@ -8,11 +8,11 @@
 <div>
   <a name="<?php print $name; ?>-preview"></a>
   <h4><?php print $name; ?></h4>
-  <details open>
+  <details>
     <summary><?php print t('Preview'); ?></summary>
     <div>
       <?php if (isset($component['description'])): ?>
-        <p><?php print $component['description'] ?></p>
+        <p><?php print $component['description']; ?></p>
       <?php endif; ?>
       <?php if (isset($component['preview'])): ?>
         <?php $html = trim(render($component['preview'])); ?>
@@ -23,17 +23,23 @@
 
       <details>
         <summary><?php print t('HTML source'); ?></summary>
-          <textarea>
-            <?php print $html; ?>
-          </textarea>
+          <pre><code>
+            <?php print check_plain($html); ?>
+          </code></pre>
       </details>
       <?php endif; ?>
       <?php if (isset($component['form'])): ?>
-        <div class="clearfix">
-          <?php $form = render($component['form']); ?>
-          <?php print $form; ?>
-          <div class="textwrapper"><textarea cols="2" rows="10"><?php print $form; ?></textarea></div>
-        </div>
+        <details>
+          <summary><?php print t('Preview'); ?></summary>
+          <?php $html = trim(render($component['form'])); ?>
+          <?php print $html; ?>
+        </details>
+        <details>
+          <summary><?php print t('HTML source'); ?></summary>
+          <pre><code>
+            <?php print check_plain($html); ?>
+          </code></pre>
+        </details>
       <?php endif; ?>
     </div>
   </details>
