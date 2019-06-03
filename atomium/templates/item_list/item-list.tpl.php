39af39f91319b442737069a431e27502546295f6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-06-03 14:34:52 +0200

    Issue #425: Update item_list component. (#426)
    * Issue #425: Update item_list component.
    
    * Issue #425: Remove code that is supposed to be for later.
    
    * Issue #425: Further improvements.
    
    * Issue #425: Add the item_list component preview.
    
    * Issue #425: Fix the indentation component.
    
    * Issue #425: Fix the menu_local_tasks component.
    
    * Issue #425: Fix CS.

--- atomium/templates/item_list/item-list.tpl.php
+++ atomium/templates/item_list/item-list.tpl.php
@@ -6,27 +6,20 @@
  */
 ?>
 <?php if ($wrapper): ?>
-<div<?php print $atomium['attributes']['wrapper']; ?>>
+  <div<?php print $atomium['attributes']['wrapper']; ?>>
 <?php endif; ?>
-
-  <?php if ($title): ?>
-    <?php print render($title); ?>
-  <?php endif; ?>
-
+  <?php print render($title); ?>
   <?php if (!empty($variables['items'])): ?>
     <?php if ($type): ?>
         <<?php print $type; ?><?php print $atomium['attributes']['list']; ?>>
     <?php endif; ?>
 
-  <?php foreach ($variables['items'] as $item): ?>
-      <?php print render($item); ?>
-  <?php endforeach; ?>
+    <?php print render($variables['items']); ?>
 
     <?php if ($type): ?>
       </<?php print $type; ?>>
     <?php endif; ?>
   <?php endif; ?>
-
 <?php if ($wrapper): ?>
-    </div>
+  </div>
 <?php endif; ?>
