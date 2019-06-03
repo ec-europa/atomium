16a8a7c047b8eae9394809f196f12c8287d99b53
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-06-03 14:58:42 +0200

    Issue #427: Update links component. (#428)

--- atomium/templates/links/links.tpl.php
+++ atomium/templates/links/links.tpl.php
@@ -5,9 +5,4 @@
  * Contains template file.
  */
 ?>
-<?php if ($links): ?>
-  <?php if (!empty($heading)): ?>
-        <<?php print $heading['level']; ?><?php print $atomium['attributes']['heading']; ?>><?php print $heading['text']; ?></<?php print $heading['level']; ?>>
-  <?php endif; ?>
-  <?php print render($links); ?>
-<?php endif; ?>
+<?php print render($links); ?>
